4020b3b21cb63253f7fbab87aed63f5cb37540dd
fscarmen <62703343+fscarmen@users.noreply.github.com>
2022-06-16 17:52:32 +0800

    Update return.sh

--- return.sh
+++ return.sh
@@ -62,7 +62,7 @@ WAN_4=$(expr "$IP_4" : '.*ip\":\"\([^"]*\).*') &&
 COUNTRY_4E=$(expr "$IP_4" : '.*country\":\"\([^"]*\).*') &&
 COUNTRY_4=$(translate "$COUNTRY_4E") &&
 ASNORG_4=$(expr "$IP_4" : '.*asn_org\":\"\([^"]*\).*') &&
-PE_4=$(curl -s4m5 ping.pe/$WAN_4) &&
+PE_4=$(curl -sm5 ping.pe/$WAN_4) &&
 COOKIE_4=$(echo $PE_4 | sed "s/.*document.cookie=\"\([^;]\{1,\}\).*/\1/g") &&
 TYPE_4=$(curl -s4m5 --header "cookie: $COOKIE_4" ping.pe/$WAN_4 | grep "id='page-div'" | sed "s/.*\[\(.*\)\].*/\1/g" | sed "s/.*orange'>\([^<]\{1,\}\).*/\1/g" | sed "s/hosting/数据中心/g;s/residential/家庭宽带/g") &&
 green " IPv4: $WAN_4\t 地区: $COUNTRY_4\t ASN: $ASNORG_4\t 类型: $TYPE_4\n"
@@ -72,7 +72,7 @@ WAN_6=$(expr "$IP_6" : '.*ip\":\"\([^"]*\).*') &&
 COUNTRY_6E=$(expr "$IP_6" : '.*country\":\"\([^"]*\).*') &&
 COUNTRY_6=$(translate "$COUNTRY_6E") &&
 ASNORG_6=$(expr "$IP_6" : '.*asn_org\":\"\([^"]*\).*') &&
-PE_6=$(curl -s6m5 ping6.ping.pe/$WAN_6) &&
+PE_6=$(curl -sm5 ping6.ping.pe/$WAN_6) &&
 COOKIE_6=$(echo $PE_6 | sed "s/.*document.cookie=\"\([^;]\{1,\}\).*/\1/g") &&
 TYPE_6=$(curl -s6m5 --header "cookie: $COOKIE_6" ping6.ping.pe/$WAN_6 | grep "id='page-div'" | sed "s/.*\[\(.*\)\].*/\1/g" | sed "s/.*orange'>\([^<]\{1,\}\).*/\1/g" | sed "s/hosting/数据中心/g;s/residential/家庭宽带/g") &&
 green " IPv6: $WAN_6\t 地区: $COUNTRY_6\t ASN: $ASNORG_6\t 类型: $TYPE_6\n"
