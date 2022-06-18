bf31a7c3c4b30fcc476fa435db76a9cd3ca87b84
fscarmen <62703343+fscarmen@users.noreply.github.com>
2022-06-18 11:53:31 +0800

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1600,7 +1600,6 @@ proxy(){
 		else
 			{ wget --no-check-certificate $CDN https://github.com/fscarmen/warp/raw/main/Client/Client_${SYSTEM}_${VERSION_ID}.deb; }&
 			[[ $SYSTEM = Debian && ! $(apt list 2>/dev/null | grep apt-transport-https) =~ installed ]] && ${PACKAGE_INSTALL[int]} apt-transport-https
-			# 如为 Ubuntu 22.04(jammy) 由于官方库暂未支持，故欺骗为20.04(focal)
 			wait
 			dpkg -i Client_${SYSTEM}_${VERSION_ID}.deb >/dev/null 2>&1
 			${PACKAGE_INSTALL[int]} -f
